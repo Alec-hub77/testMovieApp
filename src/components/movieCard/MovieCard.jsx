c13ba96347bd1d3a44d4c3958f5e50994484b0e6
Alec-hub77 <oleg.ostapiuk@yahoo.com>
2021-11-02 16:53:19 +0200

    render cards

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -5,10 +5,34 @@ import MovieModal from '../movieModal/MovieModal';
 
 const MovieCard = ({ data }) => {
   const [showModal, setShowModal] = React.useState(false);
+  const user = JSON.parse(localStorage.getItem('user'))
+
+
   return (
     <center className="card_container">
       <div className="card_wrapper">
         {showModal ? <MovieModal setShowModal={setShowModal} /> : null}
+        {data?.map((item) => (
+          <div key={item.id} className="card" onClick={() => setShowModal(true)}>
+          <div className="card_overlay">
+            <BsFillInfoCircleFill />
+          </div>
+          <div className="card_top" style={{backgroundImage: "url" + `(${item.poster})`}}>
+            <div className="card_title">
+              <h1 className="title">{item.title}</h1>
+            </div>
+          </div>
+          <div className="card_bottom">
+            <div className="card_info">
+              <ul>
+                <li>{item.genre}</li>
+                <li>{item.director}</li>
+                <li>{item.year}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        ))}
         <div className="card" onClick={() => setShowModal(true)}>
           <div className="card_overlay">
             <BsFillInfoCircleFill />
